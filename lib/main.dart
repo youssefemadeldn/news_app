9899b70d6b1a3cf1d71d92e354c4f1fd55af47c0
youssef emad eldin <youssefemadeldn>
2024-08-20 18:53:45 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/views/home_view.dart';
+
+void main() {
+  runApp(NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomPage.routName,
+      routes: {
+        HomPage.routName: (context) => HomPage(),
+      },
+    );
+  }
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomPage extends StatelessWidget {
+  static const String routName = "HomPage";
+  const HomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
