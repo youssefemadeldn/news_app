84d48b118143a0cdb3a011f4a8b294ba82caf7b7
youssef emad eldin <youssefemadeldn>
2024-08-21 17:50:38 +0300

    Finish AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: HomPage.routName,
       routes: {
         HomPage.routName: (context) => HomPage(),
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,32 +8,71 @@ class HomPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("News"),
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          image: AssetImage(
+            "assets/images/pattern.png",
+          ),
+        ),
       ),
-      body: FutureBuilder(
-        future: Api.getSources(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            showErrorDialog(context, snapshot);
-          } else if (snapshot.hasData) {
-            var listSources = snapshot.data!.sources;
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        drawer: Drawer(),
+        appBar: AppBar(
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 35,
+              ),
+            ),
+          ],
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+            size: 30,
+          ),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          )),
+          title: const Text(
+            "News APP",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.green,
+        ),
+        body: FutureBuilder(
+          future: Api.getSources(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              showErrorDialog(context, snapshot);
+            } else if (snapshot.hasData) {
+              var listSources = snapshot.data!.sources;
 
-            if (listSources!.isEmpty) {
-              return const Center(child: Text("No data available"));
+              if (listSources!.isEmpty) {
+                return const Center(child: Text("No data available"));
+              }
+
+              return ListView.builder(
+                itemCount: listSources.length,
+                itemBuilder: (context, index) =>
+                    Text(listSources[index].name ?? ""),
+              );
             }
-
-            return ListView.builder(
-              itemCount: listSources.length,
-              itemBuilder: (context, index) =>
-                  Text(listSources[index].name ?? ""),
-            );
-          }
-          return const Center(child: Text('No data found'));
-        },
+            return const Center(child: Text('No data found'));
+          },
+        ),
       ),
     );
   }
